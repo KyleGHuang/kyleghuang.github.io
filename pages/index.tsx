79655ac4de3333e1ffefad1a847ff2a7774ad656
Kyle Huang <kyleghuang@gmail.com>
2021-08-20 19:26:46 -0400

    Added 'assets' folder and adjusted navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,8 @@ import Navbar from "../components/navbar";
 
 export default function Home() {
   return (
-    <>
+    <div className="flex flex-col">
       <Navbar />
-    </>
+    </div>
   );
 }
