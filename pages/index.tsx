dd7a8497b9af43a7ce28c6a9aac1ba4aff7d1ec9
Kyle Huang <kyleghuang@gmail.com>
2021-09-05 13:58:06 -0700

    Adjusted margins

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="w-full h-full overflow-x-hidden flex flex-col justify-start items-center font-sans font-bold">
       <Navbar />
-      <div className="w-full h-full grid grid-cols-1 content-start items-center p-8 gap-8 md:p-12 md:gap-12 lg:grid-cols-2">
+      <div className="w-full h-full grid grid-cols-1 content-start items-center p-8 gap-8 md:p-12 md:gap-12 md:gap-y-72 lg:grid-cols-2 lg:mt-16 xl:w-5/6">
         <img
           src="/kyle.png"
           className="rounded-3xl shadow-xl object-cover object-top transition duration-500 ease-in-out transform hover:scale-105 w-full h-full md:object-center"
