89a2a75de2f8ef67e592a41f616fdc7d539d4633
Kyle Huang <kyleghuang@gmail.com>
2021-12-20 13:36:59 -0500

    Added hover transitions

--- components/website.tsx
+++ components/website.tsx
@@ -1,11 +1,13 @@
 export default function Website() {
   return (
     <div className="w-full h-full flex-none flex flex-col items-center">
-      <div className="w-full aspect-[16/10] bg-gray-200 flex justify-center items-center">
-        <img
-          src="/website.png"
-          className="w-5/6 shadow-md border border-black/10"
-        />
+      <div className="w-full aspect-[16/10] overflow-hidden">
+        <div className="w-full aspect-[16/10] bg-gray-200 flex justify-center items-center hover:scale-105 transition duration-500 ease-in-out">
+          <img
+            src="/website.png"
+            className="w-5/6 shadow-md border border-black/10"
+          />
+        </div>
       </div>
       <div className="w-full flex flex-row flex-wrap justify-start mt-3 mb-5 gap-3">
         <div className="bg-white border border-black py-1 px-3">Next.js</div>
