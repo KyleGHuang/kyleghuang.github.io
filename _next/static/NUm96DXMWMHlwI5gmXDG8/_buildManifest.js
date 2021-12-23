9b10a4f43a2bb1cac7fd7aa22519abe8b8259e4f
KyleGHuang <72215686+KyleGHuang@users.noreply.github.com>
2021-12-23 17:07:40 +0000

    Deploying to gh-pages from @ KyleGHuang/kyleghuang.github.io@322c83f8b79e8ac45be3494082a6cc6e30a436ea 🚀

---
+++ _next/static/NUm96DXMWMHlwI5gmXDG8/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/660-61539198f9e66ef8.js","static/chunks/pages/index-eda515eb105f55fe.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
